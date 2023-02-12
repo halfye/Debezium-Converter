0043fa1c5524054c050e70b193684cf94a933314
张玉涛 <halfye@163.com>
2023-02-12 12:26:50 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,88 @@
+## 使用方法
+`converters`参数为：自定义转换器的名字，可以随意设置。设置的值就作为转换器的名字，在以后的参数中就要使用这个名字。
+
+假设自定义的名字为`mydebeziumconverter`，则`type`参数为`mydebeziumconverter.type`。
+
+`mydebeziumconverter.type`参数为：自定义转换器的类名，必须设置。（转换器的方法）  
+
+`mydebeziumconverter.database.type`参数为：数据库类型，必须设置。（需要设置为mysql或sqlserver) 
+
+`mydebeziumconverter.format.datetime`参数为：datetime类型的格式，可选。
+
+`mydebeziumconverter.format.date`参数为：date类型的格式，可选。
+
+`mydebeziumconverter.format.time`参数为：time类型的格式，可选。
+
+> 如果仅使用mysql或sqlserver建议独立编译代码，只保留mysql或sqlserver的转换器，减少依赖。
+
+### flinkcdc
+可使用源代码也可使用编译好的jar包。只需要放入目录即可。并在配置中设置参数。
+```
+// 自定义解释器
+// 设置解释器及解释器参数
+debeziumProperties.put("converters", "mydebeziumconverter");
+debeziumProperties.put("mydebeziumconverter.type", "org.util.MyDebeziumConverter");
+debeziumProperties.put("mydebeziumconverter.database.type", "mysql");
+// 自定义格式，可选
+debeziumProperties.put("mydebeziumconverter.format.datetime", "yyyy-MM-dd HH:mm:ss");
+debeziumProperties.put("mydebeziumconverter.format.date", "yyyy-MM-dd");
+debeziumProperties.put("mydebeziumconverter.format.time", "HH:mm:ss");
+```
+### debezium
+使用jar包，并将其放在 debezium 插件的同一级别目录中。并在配置文件中设置参数。
+```
+"converters": "mydebeziumconverter",
+"mydebeziumconverter.type": "org.util.MyDebeziumConverter",
+"mydebeziumconverter.database.type": "mysql",
+"mydebeziumconverter.format.datetime": "yyyy-MM-dd HH:mm:ss",
+"mydebeziumconverter.format.date": "yyyy-MM-dd",
+"mydebeziumconverter.format.time": "HH:mm:ss"
+```
+
+## 转换器
+### mysql转换
+mysql启动时，快照期间初始化转换器，在binlog期间仍进行一次初始化转换器。（使用的类不同）
+
+|   字段类型    |     快照类型(jdbc type)      |      binlog类型(jdbc type)      |   
+|:---------:|:------------------------:|:-----------------------------:|
+|   DATE    | java.time.LocalDate(93)  |    java.time.LocalDate(91)    |  
+|   TIME    |  java.time.Duration(92)  |    java.time.Duration(92)     |  
+| DATETIME  |  java.sql.Timestamp(93)  |  java.time.LocalDateTime(93)  |
+| TIMESTAMP | java.sql.Timestamp(2014) | java.time.ZonedDateTime(2014) |
+
+### sqlserver转换
+sqlserver启动时 快照期间初始化转换器，在cdc期间不再进行初始化转换器。（使用的类相同）
+> timestamp类型在sqlserver中为byte[]类型，jdbc type为-2，因此不进行转换。
+
+|      字段类型      |          快照类型(jdbc type)           |          cdc类型(jdbc type)          |
+|----------------|:----------------------------------:|:----------------------------------:|
+|      DATE      |         java.sql.Date(91)          |         java.sql.Date(91)          |
+|      TIME      |       java.sql.Timestamp(92)       |         java.sql.Time(92)          |
+|    DATETIME    |       java.sql.Timestamp(93)       |       java.sql.Timestamp(93)       |
+|   DATETIME2    |       java.sql.Timestamp(93)       |       java.sql.Timestamp(93)       |
+| DATETIMEOFFSET | microsoft.sql.DateTimeOffset(-155) | microsoft.sql.DateTimeOffset(-155) |
+| SMALLDATETIME  |       java.sql.Timestamp(93)       |       java.sql.Timestamp(93)       |
+
+## 参考代码及文档
+### github
+主要参考大佬的代码：[debezium-datetime-converter](https://github.com/holmofy/debezium-datetime-converter)
+### debezium
+官方文档：[Debezium Engine](https://debezium.io/documentation/reference/2.1/development/engine.html)
+### 源码
+查看sqlserver、mysql的jdbc源码，了解jdbc type和java type的对应关系。
+
+## 版本说明
+1.使用的版本说明，如果不兼容，可以修改源码或提交issue。
+```
+flink-connector-debezium:2.2.0
+flink-connector-mysql-cdc:2.2.0
+flink-connector-sqlserver-cdc-2.2.1 (2.2.0不支持sqlserver的低版本，2.2.1未发布前，项目已使用，故自己编译)
+// 底层debezium版本为：1.5.4.Final
+```
+2.如果使用存在bug，积极联系哟。
+
+
+
+
+
+
---
+++ src/main/java/org/util/DebeziumConverter.java
@@ -0,0 +1,180 @@
+package org.util;
+
+import io.debezium.spi.converter.CustomConverter;
+import io.debezium.spi.converter.RelationalColumn;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+public class DebeziumConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
+    private static final Logger log = LoggerFactory.getLogger(DebeziumConverter.class);
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_FORMAT = "HH:mm:ss";
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private DateTimeFormatter dateFormatter;
+    private DateTimeFormatter timeFormatter;
+    private DateTimeFormatter datetimeFormatter;
+    private SchemaBuilder schemaBuilder;
+    private String databaseType;
+    private String schemaNamePrefix;
+
+
+    @Override
+    public void configure(Properties properties) {
+        // 必填参数：database.type。获取数据库的类型，暂时支持mysql、sqlserver
+        this.databaseType = properties.getProperty("database.type");
+        // 如果未设置，或者设置的不是mysql、sqlserver，则抛出异常。
+        if (this.databaseType == null || (!this.databaseType.equals("mysql") && !this.databaseType.equals("sqlserver"))) {
+            throw new IllegalArgumentException("database.type 必须设置为 'mysql' or 'sqlserver'");
+        }
+        // 选填参数：format.date、format.time、format.datetime。获取时间格式化的格式
+        String dateFormat = properties.getProperty("format.date", DATE_FORMAT);
+        String timeFormat = properties.getProperty("format.time", TIME_FORMAT);
+        String datetimeFormat = properties.getProperty("format.datetime", DATETIME_FORMAT);
+        // 获取自身类的包名+数据库类型为默认schema.name
+        String className = this.getClass().getName();
+        // 查看是否设置schema.name.prefix
+        this.schemaNamePrefix = properties.getProperty("schema.name.prefix", className + "." + this.databaseType);
+        // 初始化时间格式化器
+        dateFormatter = DateTimeFormatter.ofPattern(dateFormat);
+        timeFormatter = DateTimeFormatter.ofPattern(timeFormat);
+        datetimeFormatter = DateTimeFormatter.ofPattern(datetimeFormat);
+    }
+
+    // mysql的转换器
+    public void registerMysqlConverter(String columnType, ConverterRegistration<SchemaBuilder> converterRegistration) {
+        String schemaName = this.schemaNamePrefix + "." + columnType.toLowerCase();
+        schemaBuilder = SchemaBuilder.string().name(schemaName);
+        switch (columnType) {
+            case "DATE":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.time.LocalDate) {
+                        return dateFormatter.format((java.time.LocalDate) value);
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            case "TIME":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.time.Duration) {
+                        return timeFormatter.format(
+                                java.time.LocalTime.
+                                        ofNanoOfDay(((java.time.Duration) value)
+                                                .toNanos()));
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            case "DATETIME":
+            case "TIMESTAMP":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.time.LocalDateTime) {
+                        return datetimeFormatter.format((java.time.LocalDateTime) value);
+                    } else if (value instanceof java.time.ZonedDateTime) {
+                        return datetimeFormatter.format(((java.time.ZonedDateTime) value).toLocalDateTime());
+                    } else if (value instanceof java.sql.Timestamp) {
+                        return datetimeFormatter.format(((java.sql.Timestamp) value).toLocalDateTime());
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            default:
+                schemaBuilder = null;
+                break;
+        }
+    }
+
+    // sqlserver的转换器
+    public void registerSqlserverConverter(String columnType, ConverterRegistration<SchemaBuilder> converterRegistration) {
+        String schemaName = this.schemaNamePrefix + "." + columnType.toLowerCase();
+        schemaBuilder = SchemaBuilder.string().name(schemaName);
+        switch (columnType) {
+            case "DATE":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.sql.Date) {
+                        return dateFormatter.format(((java.sql.Date) value).toLocalDate());
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            case "TIME":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.sql.Time) {
+                        return timeFormatter.format(((java.sql.Time) value).toLocalTime());
+                    } else if (value instanceof java.sql.Timestamp) {
+                        return timeFormatter.format(((java.sql.Timestamp) value).toLocalDateTime().toLocalTime());
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            case "DATETIME":
+            case "DATETIME2":
+            case "SMALLDATETIME":
+            case "DATETIMEOFFSET":
+                converterRegistration.register(schemaBuilder, value -> {
+                    if (value == null) {
+                        return null;
+                    } else if (value instanceof java.sql.Timestamp) {
+                        return datetimeFormatter.format(((java.sql.Timestamp) value).toLocalDateTime());
+                    } else if (value instanceof microsoft.sql.DateTimeOffset) {
+                        return datetimeFormatter.format(
+                                ((microsoft.sql.DateTimeOffset) value).getOffsetDateTime().toLocalDateTime());
+                    } else {
+                        return this.failConvert(value, schemaName);
+                    }
+                });
+                break;
+            default:
+                schemaBuilder = null;
+                break;
+        }
+    }
+
+    @Override
+    public void converterFor(RelationalColumn relationalColumn, ConverterRegistration<SchemaBuilder> converterRegistration) {
+        // 获取字段类型
+        String columnType = relationalColumn.typeName().toUpperCase();
+        log.info("数据库:{},字段名称：{}，字段类型：{},jdbc type :{}", this.databaseType, relationalColumn.name(),columnType, relationalColumn.jdbcType());
+        // 根据数据库类型调用不同的转换器
+        if (this.databaseType.equals("mysql")) {
+            this.registerMysqlConverter(columnType, converterRegistration);
+        } else if (this.databaseType.equals("sqlserver")) {
+            this.registerSqlserverConverter(columnType, converterRegistration);
+        } else {
+            log.warn("不支持的数据库类型: {}", this.databaseType);
+            schemaBuilder = null;
+        }
+    }
+
+    private String getClassName(Object value) {
+        if (value == null) {
+            return null;
+        }
+        return value.getClass().getName();
+    }
+
+    // 类型转换失败时的日志打印
+    private String failConvert(Object value, String type) {
+        String valueClass = this.getClassName(value);
+        String valueString = valueClass==null?null:value.toString();
+        log.warn("{}类型转换失败，类型：{}，值：{}", type, valueClass, valueString);
+        return valueString;
+    }
+}
